f051b63cceec2ef3e9f221ec693ed70fd9dc4f8d
sisbhumi <87226560+sisbhumi@users.noreply.github.com>
2022-04-01 05:54:28 +0530

    7th Program

---
+++ DateFormat.py
@@ -0,0 +1,8 @@
+day = int(input("enter day: "))
+mon = int(input("enter month: "))
+yr = int(input("Enter year: "))
+
+months = [' ','January' , 'February','March','April','May','June','July','August','September','October','November','December']
+print(months[mon],day,yr)
+
+print("The date entered is: ",mon,"/",day,"/",yr)
